5590e5107a2bc44ab5f92433c27b3647c3c4ddbf
Marco Ripamonti <m.ripamonti@dkr.srl>
2023-12-07 22:18:50 +0100

    setup tsconfig and build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"] })],
   build: {
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
       formats: ["es"],
